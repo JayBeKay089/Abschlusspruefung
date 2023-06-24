b84708191801080953d605bde6fe3de5837b4d93
JayBeKay089 <jordanstyle@live.de>
2023-06-24 21:47:27 +0200

    Upload

---
+++ assets/js/animation.js
@@ -0,0 +1,34 @@
+function einblende2() {
+  var x = document.getElementById("FormSektion2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function einblende3() {
+  var x = document.getElementById("FormSektion3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// function ausblendenUndEinblenden() {
+//   var formSektion1 = document.getElementById("FormSektion1");
+//   var formSektion2 = document.getElementById("FormSektion2");
+
+//   if (formSektion1.style.display === "none") {
+//     formSektion1.style.display = "block";
+//   } else {
+//     formSektion1.style.display = "none";
+//   }
+
+//   if (formSektion2.style.display === "none") {
+//     formSektion2.style.display = "block";
+//   } else {
+//     formSektion2.style.display = "none";
+//   }
+// }
